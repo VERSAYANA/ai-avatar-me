82d9f96eb3503f27f9ef3669fc94c32a45ce586a
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-25 07:36:54 +0330

    Implement the app according to tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,98 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
+  const maxRetries = 20;
   const [input, setInput] = useState("");
+  const [img, setImg] = useState("");
+  const [retry, setRetry] = useState(0);
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [finalPrompt, setFinalPrompt] = useState("");
 
   const generateAction = async () => {
     console.log("Generating...");
+
+    // Add this check to make sure there is no double click
+    if (isGenerating && retry === 0) return;
+
+    // Set loading has started
+    setIsGenerating(true);
+
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+
+      setRetry(0);
+    }
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/jpeg",
+      },
+      body: JSON.stringify({ input }),
+    });
+
+    const data = await response.json();
+
+    if (response.status === 503) {
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      // Stop loading
+      setIsGenerating(false);
+      return;
+    }
+    setFinalPrompt(input);
+    setInput("");
+    setImg(data.image);
+    // Everything is all done -- stop loading!
+    setIsGenerating(false);
   };
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
+  // Add useEffect here
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(
+          `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`
+        );
+        setRetryCount(maxRetries);
+        return;
+      }
+
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+
+      await generateAction();
+    };
+
+    if (retry === 0) {
+      return;
+    }
+
+    runRetry();
+  }, [retry]);
+
+  console.log(img);
 
   return (
     <div className="root">
@@ -33,15 +117,30 @@ const Home = () => {
               onChange={(e) => setInput(e.target.value)}
             />
             <div className="prompt-buttons">
-              <a className="generate-button" onClick={generateAction}>
+              <a
+                className={
+                  isGenerating ? "generate-button loading" : "generate-button"
+                }
+                onClick={generateAction}
+              >
+                {/* Tweak to show a loading indicator */}
                 <div className="generate">
-                  <p>Generate</p>
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
                 </div>
               </a>
             </div>
           </div>
         </div>
       </div>
+      {img && (
+        <div className="output-content">
+          <Image src={img} width={512} height={512} alt={input} />
+        </div>
+      )}
       <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-avatar"
