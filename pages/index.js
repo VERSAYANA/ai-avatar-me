306b88db932a94408c475f79787c35ccadf632f1
Ramtin Khalatbari <VERSAYANA@outlook.com>
2023-01-25 06:12:13 +0330

    Add title, input & generate button

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,44 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+
+  const generateAction = async () => {
+    console.log("Generating...");
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Silly picture generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>
+              Turn me into anyone you want! Make sure you refer to me as
+              "versayana" in the prompt
+            </h2>
+          </div>
+          <div className="prompt-container">
+            <input
+              className="prompt-box"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={generateAction}>
+                <div className="generate">
+                  <p>Generate</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
